ca33b3cb5ce8c4ef19a780955a7767072f2a0cc3
christianbernal7 <christiandave.bernal63@gmail.com>
2024-07-05 17:37:14 +0800

    malapit na

--- Public/assets/js/main.js
+++ Public/assets/js/main.js
@@ -162,9 +162,17 @@
    */
   const glightbox = GLightbox({
     selector: '.glightbox'
+
   });
-  
+
+  /**
+   * Frequently Asked Questions Toggle
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
+
 })();
-
-
-
+  
